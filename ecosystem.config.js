1557b2ba10d471ae05db6e0ecde51c7a9a0b029a
Travis Luong <travis@travisluong.com>
2023-05-11 20:55:51 -0700

    branch level job execution

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,5 +19,18 @@ module.exports = {
       "pre-setup": "",
       ssh_options: "ForwardAgent=yes",
     },
+    staging: {
+      key: "key.pem",
+      user: "ubuntu",
+      host: "54.200.60.31",
+      ref: "origin/staging",
+      repo: "git@github.com:travisluong/my-app.git",
+      path: "/home/ubuntu",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env staging",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
+    },
   },
 };
